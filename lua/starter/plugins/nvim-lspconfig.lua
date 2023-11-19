ab5a085c7aeb8e34d64dead5afc974f4ab3da8f0
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2023-11-19 15:58:19 +0100

    Improved root_dir detection for LSP

--- lua/starter/plugins/nvim-lspconfig.lua
+++ lua/starter/plugins/nvim-lspconfig.lua
@@ -7,6 +7,7 @@ return {
   },
   config = function()
     local lspconfig = require("lspconfig")
+    local util = require("lspconfig.util")
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
     local capabilities = cmp_nvim_lsp.default_capabilities()
     local opts = { noremap = true, silent = true }
@@ -27,7 +28,10 @@ return {
         "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp",
       },
       root_dir = function(filename, _)
-        return lspconfig.util.find_git_ancestor(filename)
+        return util.root_pattern("buildServer.json")(filename)
+          or util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
+          or util.find_git_ancestor(filename)
+          or util.root_pattern("Package.swift")(filename)
       end,
     })
   end,
