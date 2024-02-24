beb3e504171f5b0532d00e4c69a6a202cb4beef1
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2024-02-24 13:49:23 +0100

    feat: improved debug bindings

--- lua/starter/plugins/nvim-dap/init.lua
+++ lua/starter/plugins/nvim-dap/init.lua
@@ -1,24 +1,31 @@
 local function setupListeners()
   local dap = require("dap")
+  local areSet = false
 
   dap.listeners.after["event_initialized"]["me"] = function()
-    vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue", noremap = true })
-    vim.keymap.set("n", "<leader>dC", dap.run_to_cursor, { desc = "Run To Cursor" })
-    vim.keymap.set("n", "<leader>ds", dap.step_over, { desc = "Step Over" })
-    vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Step Into" })
-    vim.keymap.set("n", "<leader>do", dap.step_out, { desc = "Step Out" })
-    vim.keymap.set({ "n", "v" }, "<Leader>dh", require("dap.ui.widgets").hover, { desc = "Hover" })
-    vim.keymap.set({ "n", "v" }, "<Leader>de", require("dapui").eval, { desc = "Eval" })
+    if not areSet then
+      areSet = true
+      vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue", noremap = true })
+      vim.keymap.set("n", "<leader>dC", dap.run_to_cursor, { desc = "Run To Cursor" })
+      vim.keymap.set("n", "<leader>ds", dap.step_over, { desc = "Step Over" })
+      vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Step Into" })
+      vim.keymap.set("n", "<leader>do", dap.step_out, { desc = "Step Out" })
+      vim.keymap.set({ "n", "v" }, "<Leader>dh", require("dap.ui.widgets").hover, { desc = "Hover" })
+      vim.keymap.set({ "n", "v" }, "<Leader>de", require("dapui").eval, { desc = "Eval" })
+    end
   end
 
   dap.listeners.after["event_terminated"]["me"] = function()
-    vim.keymap.del("n", "<leader>dc")
-    vim.keymap.del("n", "<leader>dC")
-    vim.keymap.del("n", "<leader>ds")
-    vim.keymap.del("n", "<leader>di")
-    vim.keymap.del("n", "<leader>do")
-    vim.keymap.del({ "n", "v" }, "<Leader>dh")
-    vim.keymap.del({ "n", "v" }, "<Leader>de")
+    if areSet then
+      areSet = false
+      vim.keymap.del("n", "<leader>dc")
+      vim.keymap.del("n", "<leader>dC")
+      vim.keymap.del("n", "<leader>ds")
+      vim.keymap.del("n", "<leader>di")
+      vim.keymap.del("n", "<leader>do")
+      vim.keymap.del({ "n", "v" }, "<Leader>dh")
+      vim.keymap.del({ "n", "v" }, "<Leader>de")
+    end
   end
 end
 
@@ -105,6 +112,7 @@ return {
         dap.terminate()
       end
       require("xcodebuild.actions").cancel()
+      dap.listeners.after["event_terminated"]["me"]()
 
       local success, dapui = pcall(require, "dapui")
       if success then
