87678f640a72e46ec4c38a67384f4ef551f6bbaa
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2024-03-15 08:02:53 +0100

    feat: simplified nvim-lint config

--- lua/starter/plugins/nvim-lint.lua
+++ lua/starter/plugins/nvim-lint.lua
@@ -1,38 +1,9 @@
-local utils = require("starter.utils")
-
 return {
   "mfussenegger/nvim-lint",
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     local lint = require("lint")
 
-    -- swiftlint
-    local pattern = "[^:]+:(%d+):(%d+): (%w+): (.+)"
-    local groups = { "lnum", "col", "severity", "message" }
-    local defaults = { ["source"] = "swiftlint" }
-    local severity_map = {
-      ["error"] = vim.diagnostic.severity.ERROR,
-      ["warning"] = vim.diagnostic.severity.WARN,
-    }
-
-    lint.linters.swiftlint = {
-      cmd = "swiftlint",
-      stdin = false,
-      args = {
-        "lint",
-        "--force-exclude",
-        "--use-alternative-excluding",
-        "--config",
-        function()
-          return utils.find_config(".swiftlint.yml") or os.getenv("HOME") .. "/.config/nvim/.swiftlint.yml" -- change path if needed
-        end,
-      },
-      stream = "stdout",
-      ignore_exitcode = true,
-      parser = require("lint.parser").from_pattern(pattern, groups, severity_map, defaults),
-    }
-
-    -- setup
     lint.linters_by_ft = {
       swift = { "swiftlint" },
     }
