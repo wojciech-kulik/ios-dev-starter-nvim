6806d2f0b88511ccdd3ca1a9abfb4c8e2e4b0f04
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2024-07-20 02:11:10 +0200

    fix: added hack to fix some issues with LSP

--- lua/starter/plugins/nvim-lspconfig.lua
+++ lua/starter/plugins/nvim-lspconfig.lua
@@ -23,6 +23,11 @@ return {
     lspconfig["sourcekit"].setup({
       capabilities = capabilities,
       on_attach = on_attach,
+      on_init = function(client)
+        -- HACK: to fix some issues with LSP
+        -- more details: https://github.com/neovim/neovim/issues/19237#issuecomment-2237037154
+        client.offset_encoding = "utf-8"
+      end,
     })
 
     -- nice icons
