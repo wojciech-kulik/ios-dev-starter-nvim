7b0b72356a94a69af800d42907db7f2aa9df588c
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2025-12-07 15:29:36 +0100

    fix: lazy loading

--- lua/starter/plugins/nvim-lspconfig.lua
+++ lua/starter/plugins/nvim-lspconfig.lua
@@ -1,6 +1,5 @@
 return {
   "neovim/nvim-lspconfig",
-  event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     { "antosha417/nvim-lsp-file-operations", config = true },
